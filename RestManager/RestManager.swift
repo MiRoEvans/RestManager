a545518d42078c9f0ae3a51735b5bbe12f9e8afc
Michael Evans (iMac) <michael.evans.us@gmail.com>
2020-05-30 19:14:03 -0500

    Results inits and CustomError.

--- RestManager/RestManager.swift
+++ RestManager/RestManager.swift
@@ -9,3 +9,84 @@
 
 import Foundation
 
+class RestManager {
+    
+    var requestHttpHeaders = RestEntity()
+    
+    var urlQueryParameters = RestEntity()
+    
+    var httpBodyParameters = RestEntity()
+    
+}
+
+extension RestManager {
+    
+    enum HttpMethod: String {
+        case get
+        case post
+        case put
+        case patch
+        case delete
+    }
+    
+    struct RestEntity {
+        private var values: [String: String] = [:]
+        
+        mutating func add(value: String, forKey key: String) {
+            values[key] = value
+        }
+        
+        func value(forKey key: String) -> String? {
+            return values[key]
+        }
+        
+        func allValues() -> [String: String] {
+            return values
+        }
+        
+        func totalItems() -> Int {
+            return values.count
+        }
+    }
+    
+    struct Response {
+        var response: URLResponse?
+        var httpStatusCode: Int = 0
+        var headers = RestEntity()
+        
+        init(fromURLResponse response: URLResponse?) {
+            guard let response = response else { return }
+            self.response = response
+            httpStatusCode = (response as? HTTPURLResponse)?.statusCode ?? 0
+            
+            if let headerFields = (response as? HTTPURLResponse)?.allHeaderFields {
+                for (key, value) in headerFields {
+                    headers.add(value: "\(value)", forKey: "\(key)")
+                }
+            }
+        }
+    }
+    
+    struct Results {
+        var data: Data?
+        var response: Response?
+        var error: Error?
+        
+        init(withData data: Data?, response: Response?, error: Error?) {
+            self.data = data
+            self.response = response
+            self.error = error
+        }
+        
+        init(withError error: Error) {
+            self.error = error
+        }
+        
+    }
+    
+    enum CustomError: Error {
+        case failedToCreateRequest
+    }
+    
+    
+}
